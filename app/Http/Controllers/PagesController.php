3cca4bd51de6e6684ed0ca4f7f8c9fcc6c4ae1e6
malihamian <z5061301@student.unsw.edu.au>
2017-05-08 20:45:57 +1000

    Showing input

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,12 +8,12 @@ use Exception;
 use App\Http\Controllers\Controller;
 class PagesController extends Controller
 {
-	public function findArticles(Request $request){
-		if($this->_verify($request) == 0){
-			return view('welcome')->with('alert', 'Please use correct input format!');
-		} else {
+               public function findArticles(Request $request){
+		//if($this->_verify($request) == 0){
+		//	return view('welcome')->with('alert', 'Please use correct input format!');
+		//} else {
 			return $this->_getArticles($request);
-		}
+		//}
 	}
 
 	private function _getArticles(Request $request){
@@ -22,8 +22,7 @@ class PagesController extends Controller
 		$startdate = $request->startdate;
 		$enddate = $request->enddate;
 
-		$input = "start_date=".$startdate."T00:00:00Z"."&end_date=".$enddate."T00:00:00Z"."&instrument_id=".$instrumentcode."&topic_codes=".$topiccode;
-
+                $input = "start_date=".$startdate."&end_date=".$enddate."&instrument_id=".$instrumentcode."&topic_codes=".$topiccode; 
 		$url = "http://139.59.224.37/api/api.cgi?".$input;
 
 		$result = file_get_contents($url);
@@ -34,26 +33,21 @@ class PagesController extends Controller
 	public function home(){
 		return view('welcome');
 	}
-
-	private function _verify(Request $request){
-		$instrumentcode = $request->instrumentcode;
+        private function _verify(Request $request){
+	        $instrumentcode = $request->instrumentcode;
 		$topiccode = $request->topiccode;
 		$startdate = $request->startdate;
 		$enddate = $request->enddate;
 		$isverified = TRUE;
-
 		if(preg_match("/^\d\d\d\d\-\d\d\-\d\d$/", $startdate) == 0 || preg_match("/^\d\d\d\d\-\d\d\-\d\d$/", $enddate) == 0){
 			$isverified = FALSE;
 		}
-
 		if(preg_match("/^[a-zA-Z]+\.[a-zA-Z]+/", $instrumentcode) == 0){
 			$isverified = FALSE;
 		}
-
 		if(preg_match("/^[a-zA-Z]+/", $topiccode) == 0){
 			$isverified = FALSE;
 		}
-
 		if($isverified == TRUE){
 			return 1;
 		} else {
