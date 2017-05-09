d56a05ac841d78d5051198acb722fe475227975b
clare ballico <clareb@clares-MacBook-Air.local>
2017-05-09 21:51:46 +1000

    can read json, but currently only using test data

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -22,12 +22,19 @@ class PagesController extends Controller
 		$startdate = $request->startdate;
 		$enddate = $request->enddate;
 
-                $input = "start_date=".$startdate."&end_date=".$enddate."&instrument_id=".$instrumentcode."&topic_codes=".$topiccode; 
+                $input = "start_date=".$startdate."&end_date=".$enddate."&instrument_id=".$instrumentcode."&topic_codes=".$topiccode;
 		$url = "http://139.59.224.37/api/api.cgi?".$input;
 
 		$result = file_get_contents($url);
-		var_dump($result);
-   	return view('data', ['articles' => $result]);
+
+		$testString ='{"NewsDataSet": [{"InstrumentIDs": "BHP.AX,BLT.L","Topic Codes": "AMERS,COM","TimeStamp": "2015-10-01T18:35:46.961Z","Headline": "UPDATE 1-Peru copper output surges again in August on Antamina, new mines","NewsText": " (Adds August production data for certain mines)LIMA"}]}';
+
+		//var_dump($result);
+
+		$newsData = json_decode($testString);
+		echo($testString);
+		var_dump($newsData);
+   return view('data', ['articles' => $newsData->NewsDataSet]);
 	}
 
 	public function home(){
--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -1,3 +1,19 @@
 @extends('layouts/layout')
 @section('content')
+	<h1>Matching Articles </h1>
+	<div class="card-columns">
+		@for($i = 0; $i<10; $i++)
+		<div  class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">{{ $articles[0]->Headline }}</h3>
+			</div>
+			<div class="panel-body"> {{ $articles[0]->NewsText }}</div>
+			<ul class="list-group">
+    <li class="list-group-item">{{ $articles[0]->InstrumentIDs }}</li>
+    <li class="list-group-item">{{ $articles[0]->{'Topic Codes'} }}</li>
+    <li class="list-group-item">{{ $articles[0]->TimeStamp }}</li>
+  </ul>
+		</div>
+		@endfor
+	</div>
 @stop
