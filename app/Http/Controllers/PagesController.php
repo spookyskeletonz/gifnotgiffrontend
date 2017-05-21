f44083a5b05257f7818baa2cf7527ad5c107a757
yuvii28 <yuvraj28singh@hotmail.com>
2017-05-21 15:57:01 +1000

    updated page controllers for rounds

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,17 +6,20 @@ use Exception;
 use App\Http\Controllers\Controller;
 class PagesController extends Controller
 {
-	public function home(){
-		return view('welcome');
-	}
+    //Display the home page
+  	public function home(){
+  		return view('welcome');
+  	}
 
+    //Get simulation for first time. Needs to:
+    //Until 10 rounds have been passed refresh page with new round number
     public function playSimulation(Request $request){
-   		return view('playSimulation');
-   	}
+        if($request->roundNumber > 10){
+            return view('results');
+        }
+        return view('playSimulation')->with('roundNumber', $request->roundNumber);
 
-   	public function getSimulation(Request $request){
-   		return view('playSimulation');
-   	}
+    }
 
    	public function results(Request $request){
    		return view('results');
