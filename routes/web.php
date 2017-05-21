1b19367932946f2af7da4009319faaf9554b7d5c
yuvii28 <yuvraj28singh@hotmail.com>
2017-05-21 13:03:22 +1000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', 'PagesController@home');
 
 Route::post('/', 'PagesController@findArticles');
+
+Route::get('/simulation', 'PagesController@getSimulation');
+
+Route::post('/simulation', 'PagesController@playSimulation');
+
+Route::get('/results', 'PagesController@results');
+
+Route::post('/results', 'PagesController@results');
+
