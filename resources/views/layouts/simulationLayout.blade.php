fd14a403f29c79b04f3a27a963bf6abef3665307
malihamian <z5061301@student.unsw.edu.au>
2017-05-24 12:58:24 +1000

    updated layout

--- resources/views/layouts/simulationLayout.blade.php
+++ resources/views/layouts/simulationLayout.blade.php
@@ -147,20 +147,144 @@
             .tt-suggestion p {
                 margin: 0;
             }
+
+            .hor_align{
+                float:right;
+            }            
         </style>
     </head>
     <body>
+
+@php
+//get formattedTime for chosenArticle
+$formattedTime = explode("T",$chosenArticle->TimeStamp)[0];
+$parts = explode("-",$formattedTime);
+$formattedTime = $parts[2]."/".$parts[1]."/".$parts[0];
+//echo $score;
+
+
+//get id of the company user is predicting for
+//echo $chosenArticle->InstrumentIDs;
+$allIDs = explode(",",$chosenArticle->InstrumentIDs);
+$chosenID = $allIDs[0]; //assumes theres at least one id
+
+//get related range for chosenArticle
+//$chosenTimestamp = mktime(0, 0, 0, $parts[0], $parts[1], $parts[2]);
+//$startRange = DateTime::createFromFormat('D, d/m/Y - H:i', $str_date););
+//$endRange = new DateTime('@' . $chosenTimestamp);
+
+
+//get related articles
+$relatedArticles = array();
+foreach ($articles as $article){
+    $articleIDs = explode(",", $article->InstrumentIDs);
+    if (in_array ($chosenID , $articleIDs) && $article != $chosenArticle){
+        //$parts = explode("T",$chosenArticle->TimeStamp)[0];
+        //$parts = explode("-",$parts);
+        //$relatedTimestamp = mktime(0, 0, 0, $parts[0], $parts[1], $parts[2]);
+        //$relatedDate = new DateTime('@' . $relatedTimestamp);
+    //  if ($relatedDate>= $startRange AND $relatedDate <= $endRange){
+            $relatedArticles[] = $article; //add to related articles
+    //  }
+        //time stuff
+
+    }
+}
+@endphp
+
+
+<div class="container">
+    
         <div class="flex-center", "position-ref" ,"full-height">
 
             <div class="content">
                  <div class="title","m-b-md">
                     Play Simulation
-										@php
-											if ($roundNumber >=1)
-												echo $prediction;
-										@endphp
+                                        @php
+                                            if ($roundNumber >=1)
+                                                echo $prediction;
+                                        @endphp
                     "<?php echo $roundNumber;?>"
         </div>
+    </div>
+</div>
+    <div class="headings">
+      <br>
+       <div class="subheadings">
+         <h1> Key Article </h1>
+       </div>
+      <br>
+     </div>
+
+   <div class="articles_left">
+    <p> {{ $chosenArticle->Headline }}</p>
+    <p> {{ $chosenArticle->NewsText }} }}</p> 
+      <ul class="list-group">
+        <li class="list-group-item">{{ $chosenArticle->InstrumentIDs }}</li>
+        <li class="list-group-item">{{$chosenArticle->{'Topic Codes'} }}</li>
+        <li class="list-group-item">{{$formattedTime }}</li>
+      </ul>
+    </div>
+    <div class="hor_align">
+        <canvas id="graph" width="400" height="400"></canvas>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+        <form class="form" method="POST" action="{{url('/simulation')}}">
+                <div id="topiccodes" class="form-group">
+                        <label>
+                            <input type="hidden" name="roundNumber" value="<?php echo $roundNumber+1?>">
+                            <input type="hidden" name="score" value="<?php echo $score?>">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        </label>
+                  </div>
+                <label>
+                  <input type="radio" name="prediction" value="increase" class="button">  Increase </label>
+                <label>
+                  <input type="radio" name="prediction" value="decrease" class="button"> Decrease </label>
+                <br>
+
+                <button type="submit" style="float:right"class="button">Continue</button>
+                <br>
+                <br>
+                <br>
+                <br>
+            </form>
+            </div>
+                <div class="headings">
+                 <br>
+                 <br>
+                 <br>
+               </div>
+                </form><canvas id="canvas" width="300" height="300"></canvas>
+                <div class="headings">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <div class="subheadings">
+                  <h1> Related Articles </h1>
+                  </div>
+                   <br>
+                 </div>
+               </div>
+
+     @foreach ($relatedArticles as $article)
+                @php
+                $formattedTime = explode("T",$article->TimeStamp)[0];
+                $parts = explode("-",$formattedTime);
+                $formattedTime = $parts[2]."/".$parts[1]."/".$parts[0];
+                @endphp
+                <div  class="panel" ,"panel-default">
+                <div class="panel-heading">
+                <h3 class="panel-title">{{ $article->Headline }}</h3>
+                </div>
+               <ul class="list-group">
+               <li class="list-group-item">{{$formattedTime }}</li>
+              </ul>
+              </div>
+             @endforeach
+
+
         @yield('content')
          <br>
          <br>
@@ -171,5 +295,36 @@
         </footer>
     </body>
 </html>
+
+
+
+
+<script>
+var labels = new Array();
+var Values = new Array();
+<?php foreach ($data as $values) : ?>
+    var text = "<?php Print($values['Date']); ?>";
+    var value = "<?php Print($values['Return']); ?>"
+    labels.push(text);
+    Values.push(value);
+<?php endforeach; ?>
+var data = {
+  labels: labels,
+  datasets: [
+    {
+      
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: Values
+    }
+  ]
+};
+var context = document.querySelector('#graph').getContext('2d');
+new Chart(context).Line(data);
+</script>
 
 
